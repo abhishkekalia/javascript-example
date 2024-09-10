e02fc61df083d8940f034f2f6aea94ebacc144a2
Abhishek Kalia <abhishek.kalia@indianic.com>
2024-09-10 19:40:09 +0530

    postgresql and javascript reading

--- capgemini.js
+++ capgemini.js
@@ -28,7 +28,7 @@ const callBy = {
     console.log("simpleFunction", this.name);
   },
   arrowFunction: () => {
-    this.lastName = "abhishek kumar kalia"
+    this.lastName = "abhishek kumar kalia";
     console.log("arrowFunction", this.lastName);
   },
 };
@@ -37,5 +37,59 @@ callBy.arrowFunction();
 
 const person = {
   name: "abhishek kumar",
-  mobileNumber: "123456789"
+  mobileNumber: "123456789",
+};
+
+/** check float value is greater or less */
+const x = 9.11;
+const y = 9.9;
+
+if (x < y) {
+  console.log("x is less than y");
+} else {
+  console.log("x is greater than y");
 }
+
+document.getElementById("parent").addEventListener("click", ()=>{
+console.log("parent clicked")
+})
+document.getElementById("child").addEventListener("click",()=>{
+  console.log("child clicked")
+})
+document.getElementById('todo-list').addEventListener('click', function(event) {
+  if (event.target && event.target.classList.contains('delete-btn')) {
+      event.target.parentElement.remove();
+  }
+})
+
+/**** Explain closures */
+ function fooa (ax){
+  return function inner  (ay){
+    return ax*ay
+  }
+}
+
+console.log("value of a",fooa(5)(10))
+// function display() {
+
+//   var f = g = 20;
+
+
+// }
+// display()
+// console.log('f',f , typeof f === 'undefined');
+
+//  console.log('g',g, typeof g === 'undefined');
+
+ function calculate(x) {
+  function multiply(y) {
+      return x * y;
+   }
+  return multiply;
+}
+ 
+const multiply3 = calculate(3);
+const multiply4 = calculate(4);
+
+console.log(multiply3);
+console.log(multiply3(3)); 
---
+++ postgresql.js
@@ -0,0 +1,17 @@
+/** To Create Database */
+
+// CREATE DATABASE databasename
+
+// CREATE DATABASE dsadsadasdas
+//     WITH
+//     OWNER = postgres
+//     ENCODING = 'UTF8'
+//     LC_COLLATE = 'C'
+//     LC_CTYPE = 'C'
+//     LOCALE_PROVIDER = 'libc'
+//     TABLESPACE = pg_default
+//     CONNECTION LIMIT = -1
+//     IS_TEMPLATE = False
+
+/**** DROP DATABASE *****/ 
+// DROP DATABASE IF EXISTS databasename;
